1dc089ae1c65643f9b117dc4cadd4a30793f0b97
pineda.juan92 <pineda.juan92@gmail.com>
2022-01-18 21:07:07 -0500

    App created to get user data and count the name of visits to the page

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+//Conexión a DB
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/recurrent_visitors",
+  { useNewUrlParser: true }
+);
+mongoose.connection.on("error", function (e) {
+  console.error(e);
+});
+
+//Schema
+const schema = {
+  name: String,
+  count: Number,
+};
+
+//Model
+const Visitor = mongoose.model("visitors", schema);
+
+//Controller
+app.get("/", async (req, res) => {
+  const name =
+    req.query.name == undefined || req.query.name == ""
+      ? "Anónimo"
+      : req.query.name;
+  if (name === "Anónimo") {
+    const visitor = new Visitor({ name, count: 1 });
+    visitor.save((err, visitor) => {});
+  } else {
+    const visitor = await Visitor.findOne({ name });
+    if (visitor) {
+      visitor.count += 1;
+      visitor.save((err, visitor) => {});
+    } else {
+      const visitor = new Visitor({ name, count: 1 });
+      visitor.save((err, visitor) => {});
+    }
+  }
+  Visitor.find((err, data) => {
+    if (err) res.status(500).send();
+    if (data.length === 0) res.status(204);
+    let table = `<table><tr><th>Id</th><th>Name</th><th>Visits</th></tr>`;
+    data.forEach((visitor) => {
+      table += `<tr><td>${visitor._id}</td><td>${visitor.name}</td><td>${visitor.count}</td></tr>`;
+    });
+    table += `</table>`;
+    res.send(table);
+  });
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
